b57479511704b03f04c6426864827ccfe07da639
robertbudijr <valenta_abram_29rpl@student.smktelkom-mlg.sch.id>
2023-02-15 10:44:39 +0700

    feat: hero section

---
+++ src/components/NavbarTop.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container container-fluid">
+      <a href="#" class="navbar-brand">
+        <img src="../assets/img/Logo.svg" alt="" />
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse align-items-center justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active btn btn--tertiary btn--lg" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn--tertiary btn--lg" href="#">Places</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn--tertiary btn--lg" href="#">Culinary</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn--tertiary btn--lg" href="#">About Us</a>
+          </li>
+        </ul>
+      </div>
+      <form class="d-flex">
+        <button class="btn btn--secondary btn--lg" type="submit">Register</button>
+        <button class="btn btn--tertiary btn--lg" type="submit">Login</button>
+      </form>
+    </div>
+  </nav>
+</template>
+<style lang="scss" scoped>
+@import '../assets/scss/styles.scss';
+.navbar {
+  padding: 0;
+  height: 67px;
+  background-color: unset !important;
+
+  .navbar-brand {
+    padding: 0;
+    margin: 0;
+  }
+
+  .navbar-nav {
+    gap: toRem(24);
+
+    .nav-item {
+      .nav-link {
+        padding: toRem(20);
+        color: $ntr100;
+
+        &:hover {
+          color: $p400;
+        }
+
+        &.active {
+          color: $p300;
+        }
+      }
+    }
+
+    .d-flex {
+      gap: toRem(16);
+    }
+  }
+}
+</style>
--- src/sections/HeroSection.vue
+++ src/sections/HeroSection.vue
@@ -1,3 +1,59 @@
+<script setup>
+import NavbarTop from '../components/NavbarTop.vue';
+</script>
+
+<template>
+  <section class="hero">
+    <navbar-top></navbar-top>
+    <div class="container">
+      <h1>
+        Looking for
+        <div class="word-translate">
+          <span>Places?</span>
+          <span>Foods?</span>
+          <span>Drinks?</span>
+        </div>
+      </h1>
+      <input class="form-control me-2" type="search" placeholder="Pura Ulun" aria-label="Search" />
+      <div class="gradient-white"></div>
+    </div>
+  </section>
+</template>
+
 <style lang="scss" scoped>
 @import '../assets/scss/styles.scss';
+section.hero {
+  padding: toRem(50) 0;
+  display: flex;
+  flex-direction: column;
+  gap: toRem(50);
+  .container {
+    background-image: url(../assets/img/hero-image.png);
+    position: relative;
+    background-size: cover;
+    overflow: hidden;
+    border-radius: toRem(20);
+    height: 583px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: toRem(16);
+
+    .gradient-white {
+      position: absolute;
+      width: 100%;
+      height: 64px;
+      bottom: 0;
+      background-image: linear-gradient(180deg, transparent, $ntr0);
+    }
+
+    h1 {
+      color: $p50;
+      .word-translate {
+        position: relative;
+      }
+    }
+  }
+}
 </style>
